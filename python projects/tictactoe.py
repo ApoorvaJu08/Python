3400c7af68b5fd86dccaa21aed60d58bafd302a8
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-28 08:46:47 +0530

    ask yes no

--- python projects/tictactoe.py
+++ python projects/tictactoe.py
@@ -21,24 +21,9 @@ Prepare yourself, human. The ultimate battle is about to begin. \n
 """
 )
 
-#Recieve and return
-def display(message):
-    print(message)
-
-def give_me_five():
-    five = 5
-    return five
-
 def ask_yes_no(question):
-    """Ask yes or no question"""
+    """Ask a yes or no question."""
     response = None
     while response not in ("y", "n"):
         response = input(question).lower()
     return response
-
-display("Here's a message for you.\n")
-number = give_me_five()
-print("Here's what I got from give_me_five():", number)
-answer = ask_yes_no("\nPlease enter 'y' or 'n': ")
-print("Thanks for entering:", answer)
-input("\n\nPress the enter key to exit.")
