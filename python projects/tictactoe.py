5bef6dfb3e63f5946322f546286c4bf2ee5c37f5
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-28 08:35:18 +0530

    recieve and return

--- python projects/tictactoe.py
+++ python projects/tictactoe.py
@@ -24,3 +24,27 @@ instructions()
 print("You probably understand the game by now.")
 input("\n\nPress the enter key to exit.")
 
+
+
+
+#Recieve and return
+def display(message):
+    print(message)
+
+def give_me_five():
+    five = 5
+    return five
+
+def ask_yes_no(question):
+    """Ask yes or no question"""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+display("Here's a message for you.\n")
+number = give_me_five()
+print("Here's what I got from give_me_five():", number)
+answer = ask_yes_no("\nPlease enter 'y' or 'n': ")
+print("Thanks for entering:", answer)
+input("\n\nPress the enter key to exit.")
