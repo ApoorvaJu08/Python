1e2107540808c48663437e8d0d3d186416f3fefe
ApoorvaJu08 <apoorva10339@gmail.com>
2019-06-28 09:07:08 +0530

    psuedo instr added

--- python projects/tictactoe.py
+++ python projects/tictactoe.py
@@ -1,3 +1,18 @@
+"""display the game instructions
+determine who goes first
+create an empty tic-tac-toe board
+display the board
+while nobody’s won and it’s not a tie
+if it’s the human’s turn
+get the human’s move
+update the board with the move
+otherwise
+calculate the computer’s move
+update the board with the move
+display the board
+switch turns
+congratulate the winner or declare a tie"""
+
 X = "X"
 O = "O"
 EMPTY = " "
