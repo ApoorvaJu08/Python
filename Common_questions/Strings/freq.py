eed515d92396a0ef1d31d0f45a9f69bdc6b9ff27
ApoorvaJu08 <apoorva10339@gmail.com>
2019-09-18 16:34:59 +0530

    freq

--- Common_questions/Strings/freq.py
+++ Common_questions/Strings/freq.py
@@ -10,5 +10,20 @@ def frequ(test_str):
     return all_freq
 
 str1 = input()  
+str2 = input()
 print ("Count of all characters in GeeksforGeeks is :\n "
                                         +  str(frequ(str1)))
+
+#to check if 2 strings are anagram or not
+res1 = frequ(str1)
+res2 = frequ(str2)
+if res1 == res2:
+    print("anagram")
+else:
+    print("not anagram")
+
+#or
+if(sorted(str1) == sorted(str2)):
+    print("Strings are anagrams")
+else:
+    print("Strings are not anagrams")
